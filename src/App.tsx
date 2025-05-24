6321ebca93a6a5ef01ab4c62f9023308c9782e04
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-05-23 21:39:38 -0300

    refactor: add layout page and showed header components on all routes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Layout from "./components/layout";
 import HomePage from "./pages/home/home";
 import LoginPage from "./pages/login/login";
 import RegisterPage from "./pages/register/register";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
   },
 ]);
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./App";
-import HeaderComponent from "./components/Header-Component/headerComponent";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HeaderComponent />
     <RouterProvider router={router} />
   </StrictMode>
 );

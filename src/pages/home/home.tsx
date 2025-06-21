8374cbd710c3d3af628adf9168ff452b5767c96c
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-06-20 23:34:05 -0300

    feat: Searching for cars registered in the database and showing them on the cars page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,56 @@
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { db } from "../../services/firebaseConection";
 import ContainerComponent from "./components/Container-Component/containerComponent";
 
+interface CarsProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImage[];
+}
+interface CarImage {
+  name: string;
+  uid: string;
+  url: string;
+}
+
 const HomePage = () => {
+  const [cars, setCars] = useState<CarsProps[]>([]);
+
+  useEffect(() => {
+    const loadCars = () => {
+      const carRefs = collection(db, "cars");
+      const queryRef = query(carRefs, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const listCars = [] as CarsProps[];
+        snapshot.forEach((doc) => {
+          listCars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCars(listCars);
+      });
+    };
+
+    loadCars();
+  }, []);
+
   return (
     <section className="flex flex-col h-screen items-center my-10 gap-10">
       {/* Input */}
@@ -20,30 +70,18 @@ const HomePage = () => {
         Carros novos e usados em todo Brasil
       </h1>
       <main className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
-        <ContainerComponent
-          imgUrl="https://www.webmotors.com.br/rbx/_next/image?url=https%3A%2F%2Fimage.webmotors.com.br%2F_fotos%2Fanunciousados%2Fgigante%2F2025%5C202506%5C20250605%5CAUDI-RS3-2.5-TFSI-GASOLINA-SPORTBACK-QUATTRO-STRONIC-wmimagem13594852611.jpg&w=640&q=75"
-          name="Audi RS3"
-          km="15.000"
-          year={2020}
-          value="269.900"
-          city="São Paulo - SP"
-        />
-        <ContainerComponent
-          imgUrl="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202410/20241017/nissan-gtr-3-8-premium-v6-24v-biturbo-gasolina-2p-automatico-wmimagem15075243340.webp?s=fill&w=552&h=414&q=60"
-          name="NISSAN GT-R"
-          km="6.730"
-          year={2015}
-          value="1.249.900"
-          city="Criciúma - SC"
-        />
-        <ContainerComponent
-          imgUrl="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202506/20250613/porsche-911-3-0-24v-h6-gasolina-carrera-gts-pdk-wmimagem13593226537.webp?s=fill&w=552&h=414&q=60"
-          name="PORSCHE 911"
-          km="1.000"
-          year={2024}
-          value="1.099.000"
-          city="Rio de Janeiro - RJ"
-        />
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <ContainerComponent
+              imgUrl={car.images[0].url}
+              name={car.name}
+              km={car.km}
+              year={car.year}
+              value={car.price}
+              city={car.city}
+            />
+          </Link>
+        ))}
       </main>
     </section>
   );
